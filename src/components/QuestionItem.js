7b501e3464d063f22867135c6d20fd88f98df696
maimuna <mohamudmaimuna003@gmail.com>
2023-07-27 11:07:15 +0300

    done

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,23 +1,34 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, deletF }) {
   const { id, prompt, answers, correctIndex } = question;
-
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
       {answer}
     </option>
   ));
-
+  function updateCorrect(e) {
+    console.log(e.target.value);
+    const newCorrect = e.target.value;
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ correctIndex: newCorrect }),
+    });
+  }
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={updateCorrect}>
+          {options}
+        </select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={() => deletF(id)}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList(questions) {
+  const questionD = questions.questions;
+  const [questionsData, setQuestionsData] = useState([]);
+  useEffect(() => {
+    setQuestionsData(questionD);
+  }, [questions]);
+  const question = questionsData.map((d, i) => {
+    return (
+      <QuestionItem
+        question={d}
+        key={i}
+        idt={Math.random()}
+        deletF={deleteList}
+      />
+    );
+  });
+  function deleteList(id) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    }).then((res) => res.json());
+    const newQD = questionsData.filter((list) => list.id !== id);
+    setQuestionsData(newQD);
+  }
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{question}</ul>
     </section>
   );
 }
